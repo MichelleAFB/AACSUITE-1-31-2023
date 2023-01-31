fe82eef2da62a8654956bf396af1bab55894dfb9
MichelleAFB <64324539+MichelleAFB@users.noreply.github.com>
2023-01-31 16:52:53 -0600

    emailjs

--- app/src/App.js
+++ app/src/App.js
@@ -2,14 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 
-//pages
-import SignIn from './pages/SignIn'
+
+/**********pages********************/
+import SigUp from './pages/SignUp'
+import SignIn from './pages/SignIn';
+
 function App() {
   return (
     <div >
       <BrowserRouter>
       <Routes>
         <Route path="/" element={<SignIn/>}/>
+        <Route path="/sign-up" element={<SignUp/>}/>
+        
       </Routes>
       </BrowserRouter>
       
