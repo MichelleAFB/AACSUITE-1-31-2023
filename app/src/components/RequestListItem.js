fdd96f6162a3841ef5d2503e5158dcf1af4c399e
MichelleAFB <64324539+MichelleAFB@users.noreply.github.com>
2023-01-31 21:13:17 -0600

    tailwind added

--- app/src/components/RequestListItem.js
+++ app/src/components/RequestListItem.js
@@ -12,7 +12,7 @@ import {setRequestModalOpen,setModalRequest} from '../redux/reservations/request
 
 //icons
 import { GrFormSubtract } from "react-icons/gr";
-import {FontAwesomeIcon}  from '@fortawesome/free-solid-svg-icons'
+
 import {FiAlertTriangle,FiX } from "react-icons/fi";
 import { TfiClose  } from "react-icons/tfi";
 import { FaHeart,FaCheck} from "react-icons/fa";
